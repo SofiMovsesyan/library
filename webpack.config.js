e92d0be8fcf1668acda3216e5c4bbcc7f79224ec
SofiMovsesyan <sofi170707@gmail.com>
2023-11-13 17:02:30 +0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: "./lib/index.js",
+    output: {
+        filename: "lib.js", 
+        path: path.resolve(__dirname, "dist"),
+    }
+}
